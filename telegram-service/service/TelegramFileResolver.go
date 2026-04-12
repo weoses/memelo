498c03ec9ab9ab8f6fd90d6d504d19f6b835f4a1
weoses <elawy3243@gmail.com>
2026-04-13 01:00:52 +0200

    telegram-service: use tmp storage to pass data between services

--- telegram-service/service/TelegramFileResolver.go
+++ telegram-service/service/TelegramFileResolver.go
@@ -2,11 +2,8 @@ package service
 
 import (
 	"context"
-	"errors"
 	"fmt"
-	"io"
 	"log/slog"
-	"net/http"
 )
 
 type BotFileGetter interface {
@@ -14,7 +11,7 @@ type BotFileGetter interface {
 }
 
 type TelegramFileResolverService interface {
-	GetFile(ctx context.Context, fileID string) ([]byte, error)
+	GetFileURL(ctx context.Context, fileID string) (string, error)
 }
 
 type TelegramFileResolverServiceImpl struct {
@@ -22,28 +19,12 @@ type TelegramFileResolverServiceImpl struct {
 	log        *slog.Logger
 }
 
-// GetFile implements TelegramFileResolverService.
-func (t *TelegramFileResolverServiceImpl) GetFile(ctx context.Context, fileID string) ([]byte, error) {
+func (t *TelegramFileResolverServiceImpl) GetFileURL(ctx context.Context, fileID string) (string, error) {
 	url, err := t.fileGetter.GetFileDirectURL(fileID)
 	if err != nil {
-		return nil, fmt.Errorf("TelegramFileResolverService: GetFileDirectURL failed, fileId: %s : %w", fileID, err)
+		return "", fmt.Errorf("TelegramFileResolverService: GetFileDirectURL failed, fileId: %s : %w", fileID, err)
 	}
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodGet, url, nil)
-	if err != nil {
-		return nil, fmt.Errorf("TelegramFileResolverService: create request failed, url: %s : %w", url, err)
-	}
-
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, fmt.Errorf("TelegramFileResolverService: download file by got url failed, url: %s : %w", url, err)
-	}
-
-	if resp.StatusCode >= 400 {
-		return nil, errors.New("TelegramFileResolverService: download file by got url failed url: %s : non 2xx status code")
-	}
-
-	return io.ReadAll(resp.Body)
+	return url, nil
 }
 
 func NewTelegramFileResolverService(fileGetter BotFileGetter) TelegramFileResolverService {
