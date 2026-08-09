9f8ebb8d6c9c8b0b2c86e309e65e50a8ef274184
weoses <elawy3243@gmail.com>
2026-08-09 17:39:13 +0200

    terraform: log found secret files

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -4,7 +4,9 @@ set -e
 for f in /etc/secrets/*; do
   echo "Found secret file: $f"
 
-  [ -f "$f" ] && . "$f"
+  ls -la "$f"
+
+  [ -f "$f" ] && export $(grep -v '^#' "$f" | xargs)
 done
 
 echo "env vars: $(env | cut -d= -f1 | sort | tr '\n' ' ')"
